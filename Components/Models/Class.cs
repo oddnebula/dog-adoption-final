cd5ac8bbd2147f9e1836c610ac4e38a30bfe9dc5
oddnebula <59035774+oddnebula@users.noreply.github.com>
2024-11-20 14:41:45 -0600

    Models

---
+++ Components/Models/Class.cs
@@ -0,0 +1,9 @@
+﻿namespace Dog Adoption.Components.Models
+{
+    public class Class
+{
+    public int Id { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastDescription { get; set; } 
+}
+}
